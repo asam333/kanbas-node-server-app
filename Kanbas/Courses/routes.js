6e50d260869fca55e0c24cd2fa331815278489ee
asam guo <asamguo@asamdeMacBook-Pro.local>
2024-08-13 01:26:11 -0700

    a6 add on

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,33 +1,37 @@
-import Database from "../Database/index.js";
+import * as dao from "./dao.js";
+
 export default function CourseRoutes(app) {
-    app.get("/api/courses", (req, res) => {
-        const courses = Database.courses;
-        res.send(courses);
-    });
-    app.post("/api/courses", (req, res) => {
-        const course = {
-            ...req.body,
-            _id: new Date().getTime().toString()
-        };
-        Database.courses.push(course);
-        res.send(course);
-    });
-    app.delete("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
-        Database.courses = Database.courses.filter(
-            (course) => course._id !== id
-        );
-        res.sendStatus(204);
+  const getAllCourses = async (req, res) => {
+    const courses = await dao.getAllCourses();
+    res.json(courses);
+  };
 
-    })
-    app.put("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
-        const course = req.body;
-        Database.courses = Database.courses.map((c) =>
-          c._id === id ? { ...c, ...course } : c
-        );
-        res.sendStatus(204);
-      });
-    
+  const createCourse = async (req, res) => {
+    const course = await dao.createCourse(req.body);
+    res.json(course);
+  };
 
+  const deleteCourse = async (req, res) => {
+    const { courseId } = req.params;
+    const status = await dao.deleteCourse(courseId);
+    res.json(status);
+  };
+
+  const updateCourse = async (req, res) => {
+    const { courseId } = req.params;
+    const status = await dao.updateCourse(courseId, req.body);
+    res.json(status);
+  };
+  // might not needed ...
+  const findCourseById = async (req, res) => {
+    const { courseId } = req.params;
+    const course = await dao.findCourseById(courseId);
+    res.json(course);
+  };
+
+  app.get("/api/courses", getAllCourses);
+  app.post("/api/courses", createCourse);
+  app.delete("/api/courses/:courseId", deleteCourse);
+  app.put("/api/courses/:courseId", updateCourse);
+  app.get("/api/courses/:courseId", findCourseById);
 }
